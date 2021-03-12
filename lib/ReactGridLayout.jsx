bcc5c38b39abff77e28d5fd309295d921a993531
Aaron Powell <Apowell829@gmail.com>
2021-03-12 18:48:37 +0000

    npm not working force build

--- lib/ReactGridLayout.jsx
+++ lib/ReactGridLayout.jsx
@@ -624,7 +624,11 @@ export default class ReactGridLayout extends React.Component<Props, State> {
     child: ReactElement<any>,
     isDroppingItem?: boolean
   ): ?ReactElement<any> {
-    console.log("called process grid item", child, this.props.toolboxItems);
+    console.log(
+      "called process grid item",
+      child,
+      this.props.toolboxItems && this.props.toolboxItems
+    );
     if (!child || !child.key) return;
     let isInToolbox = false;
     if (this.props.toolboxItems) {
